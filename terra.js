e59e25aad1980ef59c9a8937fb9e7d4feb9b75e3
arthurandael <andravinicius14@gmail.com>
2022-03-06 11:58:44 -0300

    atualização

--- terra.js
+++ terra.js
@@ -18,12 +18,12 @@ Third.addEventListener('mouseout', sairTres)
 
 
 function clicarUm() {
-  var altura = 30
-  var margem = 0
+  var altura = 80
+  var margem = 2
   First.style.background = '#6732D9';
   document.getElementById('Terra').src = 'terra.png';
-  document.getElementById('Terra').style.width = altura+"rem"
-  document.getElementById('Terra').style.margin = margem+"rem"
+  document.getElementById('Terra').style.width = altura+"%"
+  document.getElementById('Terra').style.margin = margem+"%"
   p.innerText = 'Terceiro planeta a partir do Sol e o único planeta conhecido a abrigar vida. Cerca de 29,2% da superfície da Terra é terra e os restantes 70,8% são cobertos por água. A distância da Terra ao Sol, as propriedades físicas e a história geológica permitiram que a vida evoluísse e prosperasse.'
 }
 function sairUm() {
@@ -31,14 +31,14 @@ function sairUm() {
 }
 
 function clicarDois() {
-  var altura = 21
-  var margem = 2
+  var altura = 66
+  var margem = 9
   Second.style.background = '#6732D9';
   document.getElementById('Terra').src = 'terra2.png'
-  document.getElementById('Terra').style.width = altura+"rem";
-  document.getElementById('Terra').style.margin = margem+"rem"
-  document.getElementById('Terra').style.marginLeft = 7+"rem"
-  document.getElementById('Terra').style.marginRight = 6+"rem"
+  document.getElementById('Terra').style.width = altura+"%";
+  document.getElementById('Terra').style.margin = margem+"%"
+  document.getElementById('Terra').style.marginLeft = 15+"%"
+  document.getElementById('Terra').style.marginRight = 10+"%"
   p.innerText = 'A estrutura interna da Terra é constituída por três camadas: crosta terrestre - camada superficial sólida que envolve a Terra. Tem, em média, de 30 a 40 km de espessura, mas pode ser bem mais fina ou chegar a até 80km. Possui duas partes: forma de relevo e estruturas geológicas.'
   
 
@@ -53,7 +53,7 @@ function clicarTres() {
   Third.style.background = '#6732D9';
   document.getElementById('Terra').src = 'terra.png'
   document.getElementById('Terra').style.width = altura+"%"
-  document.getElementById('Terra').style.margin = margem+"rem"
+  document.getElementById('Terra').style.margin = margem+"%"
   p.innerText = 'As estruturas geológicas representam as formações rochosas e as distintas gêneses do relevo terrestre. São subdivididas em crátons, bacias sedimentares e dobramentos modernos. Também chamadas de províncias geológicas – são as formações rochosas e estruturais que compõem a litosfera terrestre.'
 }
 function sairTres() {
